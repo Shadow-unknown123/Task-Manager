c1c55ad9b8ef3a572fc7d3d6371f222a654c74f0
Shadow <syedfarazgilani0@gmail.com>
2024-12-28 23:09:32 +0500

    changed server port from static to dynamic for vercel deployement issues

--- backend/server.js
+++ backend/server.js
@@ -49,4 +49,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(5000, () => console.log("Backend server is running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
